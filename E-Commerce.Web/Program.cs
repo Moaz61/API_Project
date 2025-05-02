9b2a0916ff4d0dc57c6950dfd655474c4815b693
moaz <moaz43700@gmail.com>
2025-05-02 23:05:54 +0300

    06-Data Seeding

--- E-Commerce.Web/Program.cs
+++ E-Commerce.Web/Program.cs
@@ -1,5 +1,7 @@
 
+using DomainLayer.Contracts;
 using Microsoft.EntityFrameworkCore;
+using Persistence;
 using Persistence.Data;
 
 namespace E_Commerce.Web
@@ -21,10 +23,15 @@ namespace E_Commerce.Web
             {
                 Options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
+            builder.Services.AddScoped<IDataSeeding, DataSeeding>();
 
             #endregion
 
             var app = builder.Build();
+
+            using var Scoope = app.Services.CreateScope();
+            var ObjectOfDataSeeding = Scoope.ServiceProvider.GetRequiredService<IDataSeeding>();
+            ObjectOfDataSeeding.DataSeed();
 
             #region Configure the HTTP request pipeline
 
