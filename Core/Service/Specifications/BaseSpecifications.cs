b62d7d9fb9f1697758b0224a62fda299ccf7278b
moaz <moaz43700@gmail.com>
2025-05-05 00:54:00 +0300

    01-Specifications

---
+++ Core/Service/Specifications/BaseSpecifications.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using DomainLayer.Contracts;
+using DomainLayer.Models;
+
+namespace Service.Specifications
+{
+    abstract class BaseSpecifications<TEntity, TKey> : ISpecifications<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        protected BaseSpecifications(Expression<Func<TEntity, bool>> CriteriaExpression)
+        {
+            Criteria = CriteriaExpression;
+        }
+
+        public Expression<Func<TEntity, bool>> Criteria {  get; private set; }
+
+        public List<Expression<Func<TEntity, object>>> IncludeExpressions { get; } = [];
+
+        protected void AddInclude(Expression<Func<TEntity, object>> includeExpression)
+        => IncludeExpressions.Add(includeExpression);
+    }
+}
