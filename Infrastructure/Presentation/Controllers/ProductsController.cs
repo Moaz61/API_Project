818f73500646db046962394de5c0ad49b2593b53
moaz <moaz43700@gmail.com>
2025-05-05 17:44:31 +0300

    03-Filtiration Specification

--- Infrastructure/Presentation/Controllers/ProductsController.cs
+++ Infrastructure/Presentation/Controllers/ProductsController.cs
@@ -15,9 +15,9 @@ namespace Presentation.Controllers
     {
         //Get All Products
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<ProductDto>>> GetAllProducts()
+        public async Task<ActionResult<IEnumerable<ProductDto>>> GetAllProducts(int? BrandId, int? TypeId)
         {
-            var products = await _serviceManager.ProductService.GetAllProductsAsync();
+            var products = await _serviceManager.ProductService.GetAllProductsAsync(BrandId , TypeId);
             return Ok(products);
         }
 
