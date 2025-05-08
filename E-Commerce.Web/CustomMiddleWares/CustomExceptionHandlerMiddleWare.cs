306858871cd7abf0a30e92878509950b68cc2eaa
moaz <moaz43700@gmail.com>
2025-05-08 20:04:14 +0300

    01-Custom Exception Middleware

---
+++ E-Commerce.Web/CustomMiddleWares/CustomExceptionHandlerMiddleWare.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json;
+using Shared.ErrorModels;
+
+namespace E_Commerce.Web.CustomMiddleWares
+{
+    public class CustomExceptionHandlerMiddleWare
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<CustomExceptionHandlerMiddleWare> _logger;
+
+        public CustomExceptionHandlerMiddleWare(RequestDelegate Next , ILogger<CustomExceptionHandlerMiddleWare> logger)
+        {
+            this._next = Next;
+            this._logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next.Invoke(httpContext);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Something Went Wrong");
+
+                //Set Status Code For Response
+                httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+                //Set Content Type To Response
+                ///httpContext.Response.ContentType = "application/json";
+
+                //Response Object
+                var Response = new ErrorToReturn()
+                {
+                    StatusCode = StatusCodes.Status500InternalServerError,
+                    ErrorMessage = ex.Message
+                };
+
+                //Return Object As JSON
+                await httpContext.Response.WriteAsJsonAsync(Response);
+            }
+        }
+    }
+}
